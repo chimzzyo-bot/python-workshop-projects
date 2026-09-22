0acf15b003a8835eb1a789a4204672ddcb8b1ab8
Chimzzy <chimzzyo@gmail.com>
2026-09-22 10:57:33 +0100

    applying_discountFunction.py

---
+++ projects/applying_discountFunction.py
@@ -0,0 +1,14 @@
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return "The price should be a number"
+
+    if not isinstance(discount, (int,float)):
+        return "The discount should be a number"
+    if price<=0:
+        return "The price should be greater than 0"
+    elif discount <0 or discount >100:
+        return "The discount should be between 0 and 100"
+    else:
+        discount=(discount/100)*price
+        return price-discount
+print("Final price:", apply_discount(120,12))
